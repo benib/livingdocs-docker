16f36a288d2c422dfa6fe98a62f64df29292649a
Christian Felder (masone) <ema@rh-productions.ch>
2016-07-01 18:28:48 +0200

    fix: add .dockerignore with installation
    the node_modules/@livingdocs/docker folder must not be included in the dockerignore. otherwise the files cannot be referenced from the Dockerfile

--- bin/install.js
+++ bin/install.js
@@ -5,7 +5,7 @@ var cpy = require('cpy')
 
 module.exports = function (project, targetPath) {
 	var sourcePath = path.resolve(path.join('node_modules', '@livingdocs', 'docker', project))
-	var files = ['docker-compose.yml', 'Dockerfile']
+	var files = ['docker-compose.yml', 'Dockerfile', '.dockerignore']
 
 	cpy(files, targetPath, {cwd: sourcePath, overwrite: true}).then(function () {
 		console.log('Docker files copied successfully')

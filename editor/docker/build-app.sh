2a8d76449fff41ea8c990fdee80e879ba6ef6472
Christian Felder (masone) <ema@rh-productions.ch>
2016-07-11 13:56:42 +0200

    chore: comment warns about local changes

--- editor/docker/build-app.sh
+++ editor/docker/build-app.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This file is managed with https://github.com/upfrontIO/livingdocs-docker
+# Local changes are discouraged as they might get overwritten
+
 set -e
 
 if [ "$RUNBUILD" == "true" ]; then

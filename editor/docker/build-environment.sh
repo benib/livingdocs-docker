bba9d5ec3b0532805f952bfd470ccb3e93a97989
Christian Felder (masone) <ema@rh-productions.ch>
2016-08-29 16:06:16 +0200

    feat: build the editor configuration dynamically
    app build and configuration build have been separated in https://github.com/upfrontIO/livingdocs-editor/pull/1131 - this change takes advantage of this behavior to generate the configuration at runtime, based on `ENVIRONMENT`.
    
     BREAKING CHANGE: requires li-editor version ^6

---
+++ editor/docker/build-environment.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This file is managed with https://github.com/upfrontIO/livingdocs-docker
+# Local changes are discouraged as they might get overwritten
+
+set -e
+
+if [ "$SKIP_BUILD" == "true" ]; then
+  echo "Skipping build..."
+else
+  echo "Building environment ${ENVIRONMENT}..."
+  npm run build:environment
+  exec "$@"
+fi

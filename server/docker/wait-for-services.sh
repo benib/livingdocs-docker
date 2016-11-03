e2f5531ca2219c7f611df408fc1e55cb71a510cc
Christian Felder (masone) <ema@rh-productions.ch>
2016-11-03 12:12:18 +0100

    fix: wait on the docker service hosts

--- server/docker/wait-for-services.sh
+++ server/docker/wait-for-services.sh
@@ -6,8 +6,8 @@
 set -e
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-$DIR/wait.sh -h ${DOCKER_IP:-localhost} -p 9200 -m "waiting for TCP connection to Elasticsearch..."
-$DIR/wait.sh -h ${DOCKER_IP:-localhost} -p 5432 -m "waiting for TCP connection to PostgreSQL..."
+$DIR/wait.sh -h ${elasticsearch} -p 9200 -m "waiting for TCP connection to Elasticsearch..."
+$DIR/wait.sh -h ${postgres} -p 5432 -m "waiting for TCP connection to PostgreSQL..."
 
 
 echo "Running command ..."
